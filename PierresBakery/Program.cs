9aefe78ae8c86914076dc3d18408dbc6a9b17a2e
TorchAblaze <tiffanygreathead@gmail.com>
2021-05-07 17:19:11 -0700

    Fix: change pluralize method to static, Add loop in Program.cs

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -6,7 +6,7 @@ namespace PierresMenu
   
   public class Program
   {
-    public string pluralize(string pluralNoun, string singularNoun, int amount)
+    public static string pluralize(string pluralNoun, string singularNoun, int amount)
     {
       if (amount == 1)
       {
@@ -19,6 +19,8 @@ namespace PierresMenu
     }
     public static void Main()
     {
+      int total = 0;
+
       Console.WriteLine(@"****************************************************************************************
            
                           Welcome to Pierre's Bakery!
@@ -31,28 +33,34 @@ Due to the effects of Covid 19, we are only offering bread and pastries at the m
 
 ****************************************************************************************");
 
-      Console.WriteLine("\nHow can we help you today? (Press 'b' to buy bread or 'p' to buy pastries)");
-      string productType = Console.ReadLine().ToLower();
-      if (productType.Contains("b"))
+      for (int index = 1; index < 3; index++)
       {
-        Console.WriteLine("\nYou've chosen to buy bread! Bread prices are: Buy 2, get 1 free.\nPlease enter the amount of loaves you would like to buy today: ");
-        int breadAmount = int.Parse(Console.ReadLine());
-        string loafWord = Program.pluralize("loaves", "loaf", breadAmount);
-        Bread breadOrder = new Bread(breadAmount);
-        Console.WriteLine($"\nYou've ordered {breadAmount} {loafWord} of bread! Your total is ${breadOrder.OrderTotal()}.00.\n");
+        Console.WriteLine("\nHow can we help you today? (Press 'b' to buy bread or 'p' to buy pastries)");
+        string productType = Console.ReadLine().ToLower();
+        if (productType.Contains("b"))
+        {
+          Console.WriteLine("\nYou've chosen to buy bread! Bread prices are: Buy 2, get 1 free.\nPlease enter the amount of loaves you would like to buy today: ");
+          int breadAmount = int.Parse(Console.ReadLine());
+          string loafWord = Program.pluralize("loaves", "loaf", breadAmount);
+          Bread breadOrder = new Bread(breadAmount);
+          total += breadOrder.OrderTotal();
+          Console.WriteLine($"\nYou've ordered {breadAmount} {loafWord} of bread! Your total for this order is ${breadOrder.OrderTotal()}.00.\n");
+        }
+        else if (productType.Contains("p"))
+        {
+          Console.WriteLine("\nYou've chosen to buy pastries! Pastry prices are: Buy 1 for $2 or 3 for $5\nPlease enter the amount of pastries you would like to buy today: ");
+          int pastryAmount = int.Parse(Console.ReadLine());
+          string pastryWord = Program.pluralize("pastries", "pastry", pastryAmount);
+          Pastry pastryOrder = new Pastry(pastryAmount);
+          Console.WriteLine($"\nYou've ordered {pastryAmount} {pastryWord}! Your total for this order is ${pastryOrder.OrderTotal()}.00.\n");
+          total += pastryOrder.OrderTotal();
+        }
+        else
+        {
+          Console.WriteLine("That was an invalid response :(");
+        }
       }
-      else if (productType.Contains("p"))
-      {
-        Console.WriteLine("\nYou've chosen to buy pastries! Pastry prices are: Buy 1 for $2 or 3 for $5\nPlease enter the amount of pastries you would like to buy today: ");
-        int pastryAmount = int.Parse(Console.ReadLine());
-        string pastryWord = Program.pluralize("pastries", "pastry", pastryAmount);
-        Pastry pastryOrder = new Pastry(pastryAmount);
-        Console.WriteLine($"\nYou've ordered {pastryAmount} {pastryWord}! Your total is ${pastryOrder.OrderTotal()}.00.\n");
-      }
-      else
-      {
-        Console.WriteLine("That was an invalid response :(");
-      }
+      Console.WriteLine($"Your entire order for today is ${total}.00\n");
     }
   }
 }
